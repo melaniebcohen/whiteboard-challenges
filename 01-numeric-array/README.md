25efbd4751c50f62de5c83e38c1d2a8291e39f5c
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-12 14:05:47 -0800

    day 1 solution

---
+++ 01-numeric-array/README.md
@@ -0,0 +1,10 @@
+[Whiteboard Challenge](https://github.com/melaniebcohen/01-node-ecosystem/blob/master/CHALLENGE.md)  
+Melanie Cohen & Christian Miller
+
+## Problem Domain
+
+Write a function that takes in a numeric array and returns an object literal with three properties:
+* the highest value
+* the lowest value
+* the average of all values
+
---
+++ 01-numeric-array/solution.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function returnObj(arr) {
+  let high = Math.max.apply(null, arr);
+  let low = Math.min.apply(null, arr);
+  let sum = arr.reduce(function(acc, cur) {
+    return acc + cur;
+  })
+  let avg = sum/arr.length;
+
+  let obj = {
+    highVal: high,
+    lowVal: low,
+    avgVal: avg,
+  }
+
+  return obj;
+}
+
+returnObj([1,2,3]);
