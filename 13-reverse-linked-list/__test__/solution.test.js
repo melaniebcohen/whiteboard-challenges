21c701c08006c6558350ac7ff6489c9d5076575d
Melanie Cohen <melaniebcohen@gmail.com>
2018-03-04 19:24:26 -0800

    complete

--- 13-reverse-linked-list/__test__/solution.test.js
+++ 13-reverse-linked-list/__test__/solution.test.js
@@ -6,7 +6,7 @@ let result = solution.reverseList(solution.linkedList)
 
 describe('Reverse Linked List', function() {
   describe('#reverseList', function() {
-    it('should return a linked list', function() {
+    it('should return a reversed linked list', function() {
       expect(result).toEqual({ val: 4, next: { val: 3, next: { val: 2, next: { val: 1, next: null} } } })
     })
   })
